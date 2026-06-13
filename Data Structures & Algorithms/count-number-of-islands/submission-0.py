395510dd8c807838c14a00482a754fe6809ad5a5
Siddharth Patel <105500935+siddharth0998@users.noreply.github.com>
2026-06-13 20:14:45 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows,cols = len(grid),len(grid[0])
+        res = 0
+        visit = set()
+
+        def bfs(r,c):
+            q = collections.deque()
+            q.append((r,c))
+            visit.add((r,c))
+            while q:
+                row,col = q.popleft()
+                direction = [[1,0],[0,1],[-1,0],[0,-1]]
+                for nr,nc in direction:
+                    r,c = row + nr,col + nc
+                    if (r in range(rows) and c in range(cols)
+                        and (r,c) not in visit and grid[r][c] == "1"):
+                        q.append((r,c))
+                        visit.add((r,c))
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1" and (i,j) not in visit:
+                    bfs(i,j)
+                    res += 1
+
+        return res
